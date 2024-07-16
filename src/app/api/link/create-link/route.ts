af7da54d835858f7376bdee2a6cfe7a9c99420a3
Avik-creator <avikm744@gmail.com>
2024-07-16 16:31:42 +0530

    Added Changes For AuthOptions.

--- src/app/api/link/create-link/route.ts
+++ src/app/api/link/create-link/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/utils/authOptions";
 import { prisma } from "@/db/db";
 import bcrypt from "bcrypt";
 import { BCRYPT_SALT_ROUNDS } from "@/lib/constants";
---
+++ src/utils/authOptions.ts
@@ -0,0 +1,86 @@
+import { NextAuthOptions } from "next-auth";
+import { prisma } from "@/db/db";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
+import bcrypt from "bcrypt";
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Please enter an email and password");
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user || !user?.password) {
+          throw new Error("No user found");
+        }
+
+        const passwordMatch = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!passwordMatch) {
+          throw new Error("Incorrect password");
+        }
+
+        return {
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          image: user.image,
+          emailVerified: user.emailVerified,
+        };
+      },
+    }),
+  ],
+  secret: process.env.SECRET,
+  session: {
+    strategy: "jwt",
+    maxAge: 15 * 24 * 60 * 60,
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        return {
+          ...token,
+          username: user.username,
+          id: user.id,
+        };
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          username: token.username,
+          id: token.id,
+        },
+      };
+    },
+  },
+  debug: process.env.NODE_ENV === "development",
+};
